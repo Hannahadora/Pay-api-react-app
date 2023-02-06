b71b4ab11338aebe03b628b45c417137c42411a5
hannah <hannahadora97@gmail.com>
2023-02-06 23:51:34 +0100

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="app-container">
+      <div className="lg:w-[75%] w-[100%] mx-auto">
+        <h3 className="font-bold text-[48px] lg:mr-[280px]">
+          We empower innovators by delivering access to the financial system
+        </h3>
+        <div className="flex items-start justify-between my-[60px]">
+          <h6 className="w-[30%] font-semibold font-[30px]">Our Vision</h6>
+          <p>
+            Our main goal is to build beautiful consumer experiences along with
+            developer-friendly infrastructure. The result is an intelligent tool
+            that gives everyone the ability to create amazing products that
+            solve big problems. We are deeply focused on democratizing financial
+            services through technology.
+          </p>
+        </div>
+        <div className="flex items-start justify-between mt-[40px]">
+          <h6 className="w-[25%] font-semibold font-[30px]">Our Business</h6>
+          <p>
+            At the core of our platform is the technical infrastructure APIs
+            that connect consumers. Our innovative product provides key insights
+            for businesses and individuals, as well as robust reporting for
+            traditional financial institutions and developers.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
