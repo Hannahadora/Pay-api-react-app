2111936f63005610e698b5b2dc9aaa3cdeb04a70
hannah <hannahadora97@gmail.com>
2023-03-12 20:28:36 +0100

    update

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,23 +1,38 @@
-
-import phoneIllustration from '../assets/home/desktop/illustration-phone-mockup.svg'
+import React, { useRef, useLayoutEffect }  from "react";
+import phoneIllustration from "../assets/home/desktop/illustration-phone-mockup.svg";
 // import mobilePhoneIllustration from '../assets/shared/desktop/phone_mockup.svg'
-import InputComponent from './InputComponent';
+import InputComponent from "./InputComponent";
+import gsap from "gsap";
 
 function HeaderComponent() {
+  const phoneRef = useRef();
+  const introRef = useRef()
+
+  useLayoutEffect(() => {
+    gsap.to(phoneRef.current, {
+      rotation: "360",
+    });
+  }, []);
+
   return (
     <div className="header">
       <div className="app-header">
-
-        <div className='app-container'>
-
-          <div className='intro-section'>
-            <div className='intro-text'>
-              <h1 className='lg:text-[80px] lg:text-[50px] text-[40px] font-bold lg:leading-[85px]'>Start building with our APIs for absolutely free.</h1>
+        <div className="app-container">
+          <div className="intro-section">
+          <div className="intro-text" ref={introRef}>
+              <h1 className="lg:text-[80px] lg:text-[50px] text-[40px] font-bold lg:leading-[85px]">
+                Start building with our APIs for absolutely free.
+              </h1>
               <InputComponent className="intro-input" />
-              <p className='mt-4 ml-6'>Have any questions? <a className='underline font-semibold' href='#'>Contact Us</a></p>
+              <p className="mt-4 ml-6">
+                Have any questions?{" "}
+                <a className="underline font-semibold" href="#">
+                  Contact Us
+                </a>
+              </p>
             </div>
-            <div className='z-[20]'>
-              <img src={phoneIllustration} alt='phone-mockup'/>
+            <div className="z-[20]" ref={phoneRef}>
+              <img src={phoneIllustration} alt="phone-mockup" />
               {/* <img src={mobilePhoneIllustration} alt='phone-mockup'/> */}
             </div>
           </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,37 @@
 import React, { useState } from "react";
 import ButtonComponent from "../components/ButtonComponent";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Signup = () => {
-  const contact = useState({});
+  const contact = {
+    first_name: "yjfyu",
+    last_name: "",
+    email: "",
+    phone_number: "",
+    password: "",
+  };
 
+  const validateContactValues = Yup.object({
+    first_name: Yup.string()
+      .min(2, "Must be 2 characters or more")
+      .required("Required"),
+    last_name: Yup.string()
+      .min(2, "Must be 2 characters or more")
+      .required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    phone_number: Yup.string().required("Required"),
+    password: Yup.string()
+      .min(8, "Must be 8 characters or more")
+      .required("Required"),
+  });
+
+  const handleFormSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  };
 
   return (
     <div className="app-container">
@@ -11,53 +39,71 @@ const Signup = () => {
         <h3 className="text-center font-medium text-[30px]">Sign In Here</h3>
 
         <div className="mt-[40px]">
-          <form className="flex flex-col">
-            <input
-              className="signup-input"
-              type="text"
-              id="first_name"
-              placeholder="First name"
-              value={contact.first_name}
-            />
-            <input
-              className="signup-input"
-              type="text"
-              id="last_name"
-              placeholder="Last name"
-              value={contact.last_name}
-            />
-            <input
-              className="signup-input"
-              type="text"
-              id="email"
-              placeholder="Email"
-              value={contact.email}
-            />
-            <input
-              className="signup-input"
-              type="text"
-              id="phone_number"
-              placeholder="Phone number"
-              value={contact.phone_number}
-            />
-            <input
-              className="signup-input"
-              type="text"
-              placeholder="Password"
-              value={contact.password}
-            />
-          
-            <div className="flex items-center">
-              <input type="checkbox" id="update" value={contact.update} />
-              <label className="ml-3" htmlFor="update">
-                I agree to terms and policies
-              </label>
-            </div>
+          <Formik
+            initialValues={contact}
+            validationSchema={validateContactValues}
+            onSubmit={(values, { setSubmitting }) =>
+              handleFormSubmit(values, { setSubmitting })
+            }
+          >
+            <Form className="flex flex-col">
+              <div className="mb-[20px]">
+                <Field
+                  className="signup-input"
+                  name="first_name"
+                  placeholder="First name"
+                />
+                <ErrorMessage name="first_name" />
+              </div>
 
-            <div className="mt-10">
-              <ButtonComponent title={"Submit"} tert={"tert-btn"} />
-            </div>
-          </form>
+              <div className="mb-[20px]">
+                <Field
+                  className="signup-input"
+                  name="last_name"
+                  placeholder="Last name"
+                />
+                <ErrorMessage className="mb-[20px]" name="last_name" />
+              </div>
+
+              <div className="mb-[20px]">
+                <Field
+                  className="signup-input"
+                  name="email"
+                  placeholder="Email"
+                />
+                <ErrorMessage className="mb-[20px]" name="email" />
+              </div>
+
+              <div className="mb-[20px]">
+                <Field
+                  className="signup-input"
+                  name="phone_number"
+                  placeholder="Phone number"
+                />
+                <ErrorMessage className="mb-[20px]" name="phone_number" />
+              </div>
+
+              <div className="mb-[20px]">
+                <Field
+                  className="signup-input"
+                  name="password"
+                  placeholder="Password"
+                />
+                <ErrorMessage name="password" />
+              </div>
+
+              <div className="flex items-center">
+                <Field as="checkbox" id="update" value={contact.update} />
+                <label className="ml-3" htmlFor="update">
+                  I agree to terms and policies
+                </label>
+              </div>
+
+              <div className="mt-10">
+                <ButtonComponent title={"Submit"} tert={"tert-btn"} />
+              </div>
+            </Form>
+          </Formik>
         </div>
       </div>
     </div>
